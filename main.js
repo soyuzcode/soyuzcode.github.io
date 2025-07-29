18afc048b0fd9b69f768746365ff4974aa77af70
Carlos563429 <carlitosfg.5051@gmail.com>
2025-07-29 10:08:17 -0600

    [🔧] Commit: Resolve bugs

--- main.js
+++ main.js
@@ -5,35 +5,37 @@ function getParams() {
     return param.get("value");
 }
 
-function requestUrl(URL){
-    // Create a new XMLHttpRequest object
-    var xhr = new XMLHttpRequest();
-
-    // Configure it: GET-request for the URL
-    xhr.open('GET', URL, true);
-
-    // Send the request over the network
-    xhr.send();
-
-    // This will be called after the response is received
-    xhr.onload = function() {
-        if (xhr.status != 200) { // analyze HTTP response status
-            console.error(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
-        } else { // show the result
-            console.log(xhr.responseText); // response is the server
-            return xhr.responseText; // return the response text
+async function requestUrl(URL) {
+    try {
+        const response = await fetch(URL);
+        if (!response.ok) {
+            throw new Error("Network response was not ok");
         }
-    };
+        const text = await response.text();
+        return text;
+    } catch (error) {
+        console.error("There was a problem with the fetch operation:", error);
+        return null;
+    }
 }
 
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", async () => {
     // Get the parameter from the URL
     var param = getParams();
 
     // If the parameter exists, set it as the model-viewer src
     if (param) {
-        var modelViewer = document.getElementById("content");
-        modelViewer.innerHTML = requestUrl("https://soyuzcode.github.io/f1_race_models/" + "embed_" + param + ".html");
+
+        const request = await requestUrl("https://soyuzcode.github.io/f1_race_models/embed_" + param + ".html")
+
+        if (!request) {
+            console.error("Failed to load the model from the URL.");
+            return;
+        }
+
+        const modelViewer = document.getElementById("content");
+        console.log("Loading cargo model with URL: " + request);
+        modelViewer.innerHTML = request;
         console.log("Cargo model loaded: " + param);
     } else {
         console.error("No model URL provided in the query string.");
