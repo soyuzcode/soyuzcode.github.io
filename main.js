7722b9bf4035d4715c0d5ce45597b82946c79194
Carlos563429 <carlitosfg.5051@gmail.com>
2025-07-29 09:45:24 -0600

    [✨] Feature: Update cargo model load
    Now we load the model using an embed code from f1_race_models

--- main.js
+++ main.js
@@ -5,6 +5,27 @@ function getParams() {
     return param.get("value");
 }
 
+function requestUrl(URL){
+    // Create a new XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+
+    // Configure it: GET-request for the URL
+    xhr.open('GET', URL, true);
+
+    // Send the request over the network
+    xhr.send();
+
+    // This will be called after the response is received
+    xhr.onload = function() {
+        if (xhr.status != 200) { // analyze HTTP response status
+            console.error(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+        } else { // show the result
+            console.log(xhr.responseText); // response is the server
+            return xhr.responseText; // return the response text
+        }
+    };
+}
+
 window.addEventListener("DOMContentLoaded", () => {
     // Get the parameter from the URL
     var param = getParams();
@@ -12,7 +33,7 @@ window.addEventListener("DOMContentLoaded", () => {
     // If the parameter exists, set it as the model-viewer src
     if (param) {
         var modelViewer = document.getElementById("content");
-        modelViewer.innerHTML = `<div class="sketchfab-embed-wrapper"> <iframe title="Cartoon Race Track Barcelona" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/eac5143c95a3482aab24a7e0ade79417/embed?autostart=1&dnt=1"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/cartoon-race-track-barcelona-eac5143c95a3482aab24a7e0ade79417?utm_medium=embed&utm_campaign=share-popup&utm_content=eac5143c95a3482aab24a7e0ade79417" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Cartoon Race Track Barcelona </a> by <a href="https://sketchfab.com/rccdesign?utm_medium=embed&utm_campaign=share-popup&utm_content=eac5143c95a3482aab24a7e0ade79417" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> RCC Design </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=eac5143c95a3482aab24a7e0ade79417" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p></div>`;
+        modelViewer.innerHTML = requestUrl("https://soyuzcode.github.io/f1_race_models/" + "embed_" + param + ".html");
         console.log("Cargo model loaded: " + param);
     } else {
         console.error("No model URL provided in the query string.");
